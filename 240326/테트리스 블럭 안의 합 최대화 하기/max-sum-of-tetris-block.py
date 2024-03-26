165f3a292f44ce79f79b520d6534f58b4b95a5fe
HyunSoo730 <102610889+HyunSoo730@users.noreply.github.com>
2024-03-26 11:36:46 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 2412ms, Memory: 29MB, Status: Passed - CodeTree

--- 240326/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240326/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -1,148 +1,48 @@
 import sys
 
-n,m = map(int, input().split())
+
+n, m = map(int, input().split())
 
 g = [list(map(int, input().split())) for _ in range(n)]
 
-boards = [
-    [
-        [1,1,1,1],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,0,0,0],
-        [1,0,0,0],
-        [1,0,0,0],
-        [1,0,0,0]
-    ],
-    [
-        [1,1,0,0],
-        [1,1,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,0,0,0],
-        [1,0,0,0],
-        [1,1,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,1,1,0],
-        [1,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,1,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [0,0,1,0],
-        [1,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [0,1,0,0],
-        [0,1,0,0],
-        [1,1,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,0,0,0],
-        [1,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,1,0,0],
-        [1,0,0,0],
-        [1,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,1,1,0],
-        [0,0,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,0,0,0],
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [0,1,1,0],
-        [1,1,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [0,1,0,0],
-        [1,1,0,0],
-        [1,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,1,0,0],
-        [0,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,0,0,0],
-        [1,1,0,0],
-        [1,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [1,1,1,0],
-        [0,1,0,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [0,1,0,0],
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,0,0,0]
-    ],
-    [
-        [0,1,0,0],
-        [1,1,1,0],
-        [0,0,0,0],
-        [0,0,0,0]
-    ]
-]
+# ! 3가지에 대해서 모든 경우 구하기
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+max_sum = 0
 
-# ! 19가지 모든 경우의 수 생각
-def isInner(x,y):
-    if 0<=x<n and 0<=y<m:
+
+def isInner(x, y):
+    if 0 <= x < n and 0 <= y < m:
         return True
     return False
 
-#!  주어진 위치에 대하여 가능한 모든 모양 탐색, 최대 합 반환
-def get_max_sum(startX,startY): # ! 현재 위치 기준으로 19개 모두 확인
-    maxSum = 0
-    for board in boards: # ! 보드 하나씩 꺼내 -> 4x4
-        nowSum = 0
-        for dx in range(4):
-            for dy in range(4):
-                if board[dx][dy] == 1: # ! 격자 부분의 합만 구해야지
-                    if isInner(startX + dx, startY + dy):
-                        nowSum += g[startX + dx][startY + dy]
-        maxSum = max(maxSum,nowSum)
-    return maxSum
 
-MAX = -int(1e9)
+def can_go(x, y):
+    global visited
+    if isInner(x, y) and (x, y) not in visited:
+        return True
+    return False
+
+
+def DFS(L, now_sum):
+    global max_sum
+    if L == 4:  # ! 4칸 확인
+        max_sum = max(max_sum, now_sum)
+    else:
+        for (x, y) in visited:  # ! 인접한 좌표들로부터 꺼내야지 -> 즉 주어진 좌표에 대해 가능한 모든 모양을 탐색
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if can_go(nx,ny): # ! 해당 좌표 내부에 있으면서 아직 방문 안했으면
+                    visited.append((nx,ny))
+                    DFS(L+1, now_sum + g[nx][ny])
+                    visited.pop()
+
+visited = []
 for x in range(n):
-    for y in range(m): # ! 현재 위치에서 매번 계산
-        get = get_max_sum(x,y)
-        MAX = max(MAX, get)
-print(MAX)
+    for y in range(m):
+        visited.append((x, y))  # ! 현재 좌표
+        DFS(1, g[x][y])
+        visited.pop()
+
+print(max_sum)
